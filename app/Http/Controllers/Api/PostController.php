c7793765ba0f3b784ecf9a59a8e467d6bea36a64
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-04-04 17:31:05 +0200

    added with postcontroller api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-       $posts = Post::all();
+       $posts = Post::all()->with('category', 'tag');
 
        return response()->json($posts);
     }
