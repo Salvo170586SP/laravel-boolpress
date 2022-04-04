20bc3627e0b4e5f86af6440950fb06d5ed607f41
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-04-04 16:46:40 +0200

    added v-if postsList

--- public/js/front.js
+++ public/js/front.js
@@ -2034,6 +2034,9 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "PostsList",
@@ -2056,7 +2059,7 @@ __webpack_require__.r(__webpack_exports__);
       })["catch"](function (err) {
         console.error(err);
       }).then(function () {
-        console.log('chiamata terminata');
+        console.log("chiamata terminata");
       });
     }
   },
@@ -37816,18 +37819,19 @@ var render = function () {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c(
-    "section",
-    { staticClass: "my-5" },
-    [
-      _c("h2", { staticClass: "mb-5" }, [_vm._v("Lista Dei Post")]),
-      _vm._v(" "),
-      _vm._l(_vm.posts, function (post) {
-        return _c("PostCard", { key: post.id, attrs: { post: post } })
-      }),
-    ],
-    2
-  )
+  return _c("section", { staticClass: "my-5" }, [
+    _c("h2", { staticClass: "mb-5" }, [_vm._v("Lista Dei Post")]),
+    _vm._v(" "),
+    _vm.posts.length
+      ? _c(
+          "div",
+          _vm._l(_vm.posts, function (post) {
+            return _c("PostCard", { key: post.id, attrs: { post: post } })
+          }),
+          1
+        )
+      : _c("p", [_vm._v("Non ci sono post")]),
+  ])
 }
 var staticRenderFns = []
 render._withStripped = true
--- resources/js/components/posts/PostsList.vue
+++ resources/js/components/posts/PostsList.vue
@@ -1,42 +1,48 @@
 <template>
   <section class="my-5">
-      <h2 class="mb-5">Lista Dei Post</h2>
-        <PostCard v-for="post in posts" :key="post.id" :post="post"/>
+    <h2 class="mb-5">Lista Dei Post</h2>
+    <div v-if="posts.length">
+      <PostCard v-for="post in posts" :key="post.id" :post="post" />
+    </div>
+    <p v-else>Non ci sono post</p>
   </section>
 </template>
 
 <script>
-import PostCard from './PostCard.vue';
+import PostCard from "./PostCard.vue";
 export default {
-    name: "PostsList",
-    components: {
-        PostCard,
+  name: "PostsList",
+  components: {
+    PostCard,
+  },
+  data() {
+    return {
+      posts: [],
+      url: "http://localhost:8000/api/posts",
+    };
+  },
+  methods: {
+    //CHIAMATA DELL API CON AXIOS
+    getPosts() {
+      axios
+        .get(this.url)
+        .then((res) => {
+          this.posts = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+        .then(() => {
+          console.log("chiamata terminata");
+        });
     },
-    data(){
-        return{
-            posts: [],
-            url: "http://localhost:8000/api/posts",
-        }
-    },
-    methods:{
-        //CHIAMATA DELL API CON AXIOS
-        getPosts(){
-            axios.get(this.url).then((res)=>{
-              this.posts = res.data;
-            }).catch((err)=>{
-                console.error(err);
-            }).then(()=>{
-                console.log('chiamata terminata');
-            })
-        }
-    },
-    mounted(){
-        //CHIAMO L'API
-        this.getPosts();
-    }
-}
+  },
+  mounted() {
+    //CHIAMO L'API
+    this.getPosts();
+  },
+};
 </script>
 
 <style>
-
 </style>
